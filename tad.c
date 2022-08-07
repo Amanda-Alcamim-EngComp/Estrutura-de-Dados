3cbd45e2f07f6fe2a1a4f85925750cc80db261a9
Amanda Alcamim - Faculdade <amanda.alcamim@estudante.ifms.edu.br>
2022-08-06 21:08:30 -0400

    Main

---
+++ tad.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "float_vector.h"
+
+int main(int argc, char const *argv[]){
+     Float_Vector *vector = create(10);
+    printf(capacity);
+
+
+     return 0;
+}
+
+
+
+
+void append(Float_Vector *vector, float n);
+int capacity(const Float_Vector *vector); 
