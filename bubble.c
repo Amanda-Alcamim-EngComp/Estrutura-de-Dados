afc1541d8a64602c0c2d8eef0fcae225e4b44e0a
Amanda Alcamim - Faculdade <amanda.alcamim@estudante.ifms.edu.br>
2022-08-08 14:43:56 -0400

    Exercício de Ordenação

---
+++ bubble.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main()
+{
+    int vector = {7, 3, 0, 1, 3, 8, 2, 9};
+    bubble(vector, 8);
+
+    return 0;
+}
+
+void swap(int *a, int *b)
+{
+    int aux = *a;
+    *a = *b;
+    *b = aux;
+}
+
+void bubble(int vector[], int size)
+{    
+    for(int i = 0; i < size - 1; i++)
+    {
+        for(int j = 0; j < size -1; j++)
+        {
+            if(vector[j] > vector[j+1])
+            {
+            swap(&vector[j], &vector[j+1]);
+            }   
+        }
+        
+    }
+}
+
+// void printVector(conts int *vector){
+
+// }
