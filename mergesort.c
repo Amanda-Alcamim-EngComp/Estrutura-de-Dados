797d746e6adc9703d086887f4b0a00e0119de45f
Amanda Alcamim - Faculdade <amanda.alcamim@estudante.ifms.edu.br>
2022-08-19 18:28:29 -0400

    Ordenação Merge - Num. Aleatórios

--- mergesort.c
+++ mergesort.c
@@ -3,7 +3,7 @@
 #include <string.h>
 #include "ordenacao.h"
 #define TAM 8
-
+ 
 
 int main()
 {
@@ -28,7 +28,51 @@ int main()
 void merge(int arr[], int inicio, int meio, int fim)
 {
     int n1 = meio - inicio + 1;
+    int n2 = fim - meio;
 
+
+    int L[n1];  //sub vetor da esquerda
+    int R[n2];  //sub vetor da direita
+
+    for(int i = 0; i < n1; i++)
+    {
+        L[i] = arr[inicio + i];
+    }
+
+    for(int i = 0; i < n2; i++)
+    {
+        R[i] = arr[meio + i];  //pensar...
+    }
+
+    int i = 0, j = 0, k = 0;   //i=esquerda; j=direita; k=vetor completo
+    while(i < n1 &&  j < n2)
+    {
+        if(L[i] <= R[j])
+        {
+            arr[k] = L[i];
+            i++;
+        }
+        else
+        {
+            arr[k] = R[j];
+            j++;
+        }
+        k++;
+    }
+
+    while(j < n2)
+    {
+        arr[k] = R[j];
+        j++;
+        k++;
+    }
+    
+    while(i < n1)
+    {
+        arr[k] = L[i];
+        i++;
+        k++;
+    }
 }
 
 void print(int array[])
